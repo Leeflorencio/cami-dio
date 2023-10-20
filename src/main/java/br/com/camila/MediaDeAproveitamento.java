852f7179e0cf604096e9462af97d27e3a4356e93
leticiaflorencio <florenciolee63@gmail.com>
2023-10-20 00:03:01 -0300

    exercicio media de aproveitamento

---
+++ src/main/java/br/com/camila/MediaDeAproveitamento.java
@@ -0,0 +1,46 @@
+package br.com.camila;
+
+import java.util.Scanner;
+
+public class MediaDeAproveitamento {
+
+    /*
+        Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina
+        ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+        Média de Aproveitamento  Conceito
+          Entre 9.0 e 10.0        A
+          Entre 7.5 e 9.0         B
+          Entre 6.0 e 7.5         C
+          Entre 4.0 e 6.0         D
+          Entre 4.0 e zero        E
+     */
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a nota 1: ");
+        Double nota1 = sc.nextDouble();
+
+        System.out.println("Digite a nota 2: ");
+        Double nota2 = sc.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+        String conceito = "";
+
+        if(media >= 9.0 && media <= 10.0){
+            conceito = "A";
+        } else if (media >= 7.5 && media <= 9.0 ) {
+            conceito = "B";
+        } else if (media >= 6.0 && media <= 7.5) {
+            conceito = "C";
+        } else if (media >= 4.0 && media <= 6.0) {
+            conceito = "D";
+        } else if (media >= 0 && media <= 4.0){
+            conceito = "E";
+        }
+
+        System.out.println("Média: " + media);
+        System.out.println("Conceito " + conceito);
+    }
+}
